6dc3d70c8802a5ef3cae7c0077934bc0f34bdce4
Swicki <wmswicki@email.neit.edu>
2025-01-09 09:15:51 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+//middleware to serve static data
+app.use(express.static(path.join(__dirname, "public")));
+
+let message = "Wouldn't you like to be a Pepper to?";
+
+function sendMessage()
+{
+    console.log(message);
+}
+
+// sendMessage();
+
+//route ex
+app.get("/",function(request, responce){
+    // responce.send("Hello Everyone!");
+    responce.sendFile(path.join(__dirname,"public","index.html"));
+});
+
+app.get("/testjson",(req, res)=>{
+    res.sendFile(path.join(__dirname,"public","json/games.json"));
+});
+
+setTimeout(()=>{
+    console.log("Hello 2 seconds later")
+},2000);
+
+setTimeout(()=>{
+    console.log("Hello now")
+},0);
+
+
+app.listen(port, function(){
+    console.log(`Server is running on port: ${port}`);
+});
