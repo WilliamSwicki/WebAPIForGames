e5a19ed4d76eb56c22ffa4962d0d19aeadd30074
WilliamSwicki <wmswicki@email.neit.edu>
2025-01-16 15:20:19 -0500

    aaaa

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const path = require("path");
+ const express = require("express");
+ const path = require("path");
 
 const app = express();
 const port = 3000;
 
-//middleware to serve static data
-app.use(express.static(path.join(__dirname, "public")));
+ //middleware to serve static data
+ app.use(express.static(path.join(__dirname, "public")));
 
-let message = "Wouldn't you like to be a Pepper to?";
+ let message = "Wouldn't you like to be a Pepper to?";
 
-function sendMessage()
-{
-    console.log(message);
-}
+ function sendMessage()
+ {
+     console.log(message);
+ }
 
 // sendMessage();
 
-//route ex
-app.get("/",function(request, responce){
-    // responce.send("Hello Everyone!");
-    responce.sendFile(path.join(__dirname,"public","index.html"));
-});
+ //route ex
+ app.get("/",function(request, responce){
+     // responce.send("Hello Everyone!");
+     responce.sendFile(path.join(__dirname,"public","app.js"));
+ });
 
-app.get("/testjson",(req, res)=>{
-    res.sendFile(path.join(__dirname,"public","json/games.json"));
-});
+ app.get("/testjson",(req, res)=>{
+     res.sendFile(path.join(__dirname,"public","json/games.json"));
+ });
 
 setTimeout(()=>{
     console.log("Hello 2 seconds later")
@@ -35,6 +35,6 @@ setTimeout(()=>{
 },0);
 
 
-app.listen(port, function(){
-    console.log(`Server is running on port: ${port}`);
-});
+ app.listen(port, function(){
+     console.log(`Server is running on port: ${port}`);
+ });
