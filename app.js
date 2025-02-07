21daacd16287339c4da64078d3cc6cca9ec2e86a
WilliamSwicki <wmswicki@email.neit.edu>
2025-02-06 20:50:16 -0500

    Completed assignment 4

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const { register } = require("module");
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -42,6 +45,9 @@ const User = mongoose.model("User", userSchema, "users");
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -60,6 +66,18 @@ const User = mongoose.model("User", userSchema, "users");
      console.log(message);
  }
 =======
+app.use(session({
+    secret:"12345",
+    resave:false,
+    saveUninitialized:false,
+    cookie:{secure:false}// Set to true is using https
+}));
+
+function isAuthenticated(req,res, next){
+    if(req.session.user)return next();
+    return res.redirect("/login");
+}
+
 app.use(session({
     secret:"12345",
     resave:false,
@@ -215,6 +233,31 @@ app.post("/register", async (req,res)=>{
     }
 });
 
+app.get("/register", (req,res)=>{
+    res.sendFile(path.join(__dirname, "public", "register.html"));
+})
+
+app.post("/register", async (req,res)=>{
+    try{
+        const {username, password, email} = req.body;
+
+        const existingUser = await User.findOne({username});
+
+        if(existingUser){
+            return res.send("<p>Username already taken. Try a different one</p><br><a href='/register.html'>Back</a></li>")
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const newUser = new User({username, password:hashedPassword, email});
+        await newUser.save();
+
+        res.redirect("/login");
+
+    }catch(err){
+        res.status(500).send("Error registering new user.");
+    }
+});
+
  //route ex
  app.get("/index",function(request, responce){
      // responce.send("Hello Everyone!");
@@ -225,10 +268,13 @@ app.post("/register", async (req,res)=>{
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
  app.get("/addEntry",function(request, responce){
     // responce.send("Hello Everyone!");
     responce.sendFile(path.join(__dirname,"public","addEntry.html"));
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -298,9 +344,12 @@ app.get("/login",function(request, responce){
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
  app.get("/testjson",(req, res)=>{
      res.sendFile(path.join(__dirname,"public","json/games.json"));
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
